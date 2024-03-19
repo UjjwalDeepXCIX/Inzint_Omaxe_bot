4aa0d98b373c503bcff713007d4674b4caeaf8bf
Pratham Sharma <118199100+PrathamSharma-2811@users.noreply.github.com>
2024-03-19 10:46:37 +0530

    Add files via upload
    Updated version of main script and 500 epoch trained model h5 file

--- Omaxe_BERT_TRAIN_test_Pratham/bert_2_test.py
+++ Omaxe_BERT_TRAIN_test_Pratham/bert_2_test.py
@@ -7,25 +7,28 @@ from transformers import BertTokenizer, BertForSequenceClassification
 from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
 from flask import Flask, render_template, request, jsonify
-
+from langdetect import detect
+from converter import hinglish_to_english
 app = Flask(__name__)
 
-with open('new_data.json') as file:
+with open('18_data.json') as file:
     data = json.load(file)
 
 # Load fine-tuned BERT model and tokenizer
-model_path = 'fine_tuned_bert'  # Adjust the path accordingly
+model_path = 'bert_final'  
 tokenizer = BertTokenizer.from_pretrained(model_path)
 model = BertForSequenceClassification.from_pretrained(model_path)
 label_encoder = LabelEncoder()
 
-# Sample input
-# input_text = input("enter-> ")
-
-# Tokenize input
-
-# Find the intent from the JSON data
 def gen_response(question):
+
+    # Detect the language of the input text
+    detected_language = detect(question)
+        
+    if detected_language != 'en':
+        question = hinglish_to_english(question)
+        
+    
     inputs = tokenizer(question, return_tensors="pt")
     input_ids = inputs["input_ids"]
     attention_mask = inputs["attention_mask"]
@@ -35,29 +38,28 @@ def gen_response(question):
         outputs = model(input_ids=input_ids, attention_mask=attention_mask)
 
     # Get predicted label
+    
     predicted_label_idx = torch.argmax(outputs.logits).item()
     intents = [intent['tag'] for intent in data['intents']]
 
     # Initialize and fit LabelEncoder
     label_encoder = LabelEncoder()
     label_encoder.fit(intents)
-    # print(label_encoder.classes_)
-    predicted_label = label_encoder.classes_[predicted_label_idx] if predicted_label_idx < len(label_encoder.classes_) else "Unknown"
-    # predicted_label = label_encoder.classes_[predicted_label_idx] if predicted_label_idx < len(label_encoder.classes_) else "Unknown"
-
-    # Now you can use the label encoder for inverse transformation
-    predicted_label = label_encoder.inverse_transform([predicted_label_idx])[0]
-
-    response = "Sorry, I couldn't understand your question."
-    for intent in data['intents']:
-        if intent['tag'] == predicted_label:
-            # Randomly select a response from the list of responses
-            response = intent['responses']
+    # print(predicted_label_idx,label_encoder.classes_.shape[0])
+    if predicted_label_idx < len(label_encoder.classes_):
+        # Perform inverse transformation if predicted label index is within range
+        predicted_label = label_encoder.inverse_transform([predicted_label_idx])[0]
+       
+        
+        for intent in data['intents']:
+            if intent['tag'] == predicted_label:
+                response = intent['responses']
+                return response
+           
+    else:
+            response = "Sorry, I can answer only questions relevant to Omaxe"
             return response
 
-# print("Input:", input_text)
-# print("Predicted Intent:", predicted_label)
-# print("Response:", response)
 
 
 @app.route("/")
@@ -71,3 +73,4 @@ def chat():
 
 if __name__ == '__main__':
     app.run()
+    
